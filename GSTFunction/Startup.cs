1bab9e0900d08894410b05811d080b59891dc610
Kim Del Rosario <vam_kim@hotmail.com>
2021-05-31 00:02:29 +1200

    Added the configuration for the injection to be able to run the function and set a Percentage value for the GST which can be changed anytime.

---
+++ GSTFunction/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(GSTFunction.Startup))]
+namespace GSTFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton(config);
+            services.AddSingleton<IXmlProcessService, XMLProcessService>();
+            return services;
+
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.setting.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            var configReader = configBuilder.Build();
+
+            var GSTPersentage = configReader["GSTpercentage"];
+            double convertedGSTPersentage;
+            if (String.IsNullOrEmpty(GSTPersentage) || !double.TryParse(GSTPersentage, out convertedGSTPersentage))
+            {
+                throw new Exception("GST Percentage is not maintained properly in the configuration");
+            }
+            
+            var serviceProvider = ConfigureServices(builder.Services, configReader).BuildServiceProvider(true);
+        }
+
+    }
+}
